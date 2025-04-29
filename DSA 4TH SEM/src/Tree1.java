14682c648f7d3670af6d9257b71afc6a821ab4fc
vanshulgarg18 <145276057+vanshulgarg18@users.noreply.github.com>
2025-04-29 18:26:29 +0530

    Update Tree1.java

--- DSA 4TH SEM/src/Tree1.java
+++ DSA 4TH SEM/src/Tree1.java
@@ -1,42 +1,33 @@
 public class Tree1 {
-
     static class TreeNode {
         int data;
-        TreeNode left, right;
-
-        TreeNode(int value) {
-            data = value;
-            left = right = null;
+        TreeNode l, r;
+        TreeNode(int v) {
+            data = v;
+            l = r = null;
         }
     }
-
     public static int height(TreeNode root) {
         if (root == null)
             return 0;
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-
-        return Math.max(leftHeight, rightHeight) + 1;
+        int lefthe = height(root.l);
+        int righthe = height(root.r);
+        return Math.max(lefthe, righthe) + 1;
     }
-
-    public static int countNodes(TreeNode root) {
+    public static int cntnodes(TreeNode root) {
         if (root == null)
             return 0;
-        int leftNodes = countNodes(root.left);
-        int rightNodes = countNodes(root.right);
-        return 1 + leftNodes + rightNodes;
-
+        int leftN = cntnodes(root.l);
+        int rightN = cntnodes(root.r);
+        return 1 + leftN + rightN;
     }
-
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-
-        System.out.println("Height of the tree is : " + height(root));
-
-        System.out.println("Number of the nodes in the tree is : " + countNodes(root));
+        root.l = new TreeNode(2);
+        root.r = new TreeNode(3);
+        root.l.l = new TreeNode(4);
+        root.l.r = new TreeNode(5);
+        System.out.println(height(root));
+        System.out.println(cntnodes(root));
     }
 }
