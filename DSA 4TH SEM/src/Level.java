9ddf4eacd768f41cc125bd0c91123d5a64c0b6ac
vanshulgarg18 <145276057+vanshulgarg18@users.noreply.github.com>
2025-04-29 18:16:49 +0530

    Update Level.java

--- DSA 4TH SEM/src/Level.java
+++ DSA 4TH SEM/src/Level.java
@@ -1,62 +1,50 @@
 public class Level {
-
     static class TreeNode {
         int data;
-
-        TreeNode left, right;
-
+        TreeNode l, r;
         TreeNode(int val) {
             data = val;
-            left = right = null;
+            l = r = null;
         }
     }
-
     public static void printLevel(TreeNode root, int level) {
         if (root == null)
             return;
-
         if (level == 1) {
             System.out.print(root.data + " ");
-        } else {
-            printLevel(root.left, level - 1);
-            printLevel(root.right, level - 1);
+        } 
+        else {
+            printLevel(root.l, level - 1);
+            printLevel(root.r, level - 1);
         }
-
     }
-
     public static void insertData(TreeNode root, int value) {
         if (root == null) {
             root = new TreeNode(value);
         }
-
-        else if (root.left == null) {
-            root.left = new TreeNode(value);
-        } else if (root.right == null) {
-            root.right = new TreeNode(value);
-        } else {
-            insertData(root.left, value);
-            insertData(root.right, value);
+        else if (root.l == null) {
+            root.l = new TreeNode(value);
+        } 
+        else if (root.r == null) {
+            root.r = new TreeNode(value);
+        } 
+        else {
+            insertData(root.l, value);
+            insertData(root.r, value);
         }
-
     }
-
     public static void printTree(TreeNode root) {
         if (root == null)
             return;
-        printTree(root.left);
+        printTree(root.l);
         System.out.print(root.data + " ");
-        printTree(root.right);
+        printTree(root.r);
     }
-
     public static void main(String[] args) {
         TreeNode root = new TreeNode(100);
-        root.left = new TreeNode(50);
-        root.right = new TreeNode(150);
-
-        root.left.right = new TreeNode(56);
-
+        root.l = new TreeNode(50);
+        root.r = new TreeNode(150);
+        root.l.r = new TreeNode(56);
         insertData(root, 2);
-        // System.out.println(root);
     }
-
 }
