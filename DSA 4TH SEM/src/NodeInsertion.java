4b6d6f77f74ed19a0983659f3105596a14749a4c
vanshulgarg18 <145276057+vanshulgarg18@users.noreply.github.com>
2025-04-29 18:12:47 +0530

    Update NodeInsertion.java

--- DSA 4TH SEM/src/NodeInsertion.java
+++ DSA 4TH SEM/src/NodeInsertion.java
@@ -5,85 +5,68 @@ public class NodeInsertion {
 
     static class TreeNode {
         int data;
-        TreeNode left, right;
+        TreeNode l, r;
 
         TreeNode(int val) {
             data = val;
-            left = right = null;
+            l = r = null;
         }
     }
-
     public static void insertData(TreeNode root, int value) {
         if (root == null) {
             root = new TreeNode(value);
-        } else if (root.left == null) {
-            root.left = new TreeNode(value);
-        } else if (root.right == null) {
-            root.right = new TreeNode(value);
-        } else {
-            insertData(root.left, value);
+        } 
+        else if (root.l == null) {
+            root.l = new TreeNode(value);
+        } 
+        else if (root.r == null) {
+            root.r = new TreeNode(value);
+        } 
+        else {
+            insertData(root.l, value);
         }
     }
-
     public static void printPreorder(TreeNode root) {
         if (root != null) {
             System.out.print(root.data + " ");
-            printPreorder(root.left);
-            printPreorder(root.right);
+            printPreorder(root.l);
+            printPreorder(root.r);
         }
     }
-
     public static void printInorder(TreeNode root) {
         if (root != null) {
-            printInorder(root.left);
+            printInorder(root.l);
             System.out.print(root.data + " ");
-            printInorder(root.right);
+            printInorder(root.r);
         }
     }
-
     public static void insert(TreeNode root, int key){
         Queue<TreeNode>queue = new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()){
             TreeNode temp = queue.poll();
-            if(temp.left==null){
-                temp.left = new TreeNode(key);
+            if(temp.l==null){
+                temp.l = new TreeNode(key);
                 break;
-            }else queue.add(temp.left);
-
-            if(temp.right==null){
-                temp.right = new TreeNode(key);
+            }
+            else queue.add(temp.l);
+            if(temp.r==null){
+                temp.r = new TreeNode(key);
                 break;
-            }else queue.add(temp.right);
+            }
+            else queue.add(temp.r);
         }
     }
-
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10); // root node
-        // insertData(root, 20);
-        // insertData(root, 30);
-        // insertData(root, 40);
-        // insertData(root, 50);
-
-        root.left = new TreeNode(20);
-        root.right = new TreeNode(30);
-
+        TreeNode root = new TreeNode(10);
+        root.l = new TreeNode(20);
+        root.r = new TreeNode(30);
         System.out.println("Before Insertion : ");
-
         printInorder(root);
-
         insertData(root,40);
         insertData(root, 100);
         System.out.println();
-
         System.out.println("After Insertion : ");
-
         printInorder(root);
-
-        // System.out.println("Preorder Traversal:");
-        // printPreorder(root);
-
-        // System.out.println("\nInorder Traversal:");
-        // printInorder(root);
     }
 }
